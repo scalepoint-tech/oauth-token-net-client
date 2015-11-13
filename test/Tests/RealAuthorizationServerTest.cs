84aa795793fe37c980f3b73ab8d1532e67106259
Yuriy Ostapenko <uncleyo@users.noreply.github.com>
2015-11-13 14:27:39 +0100

    Add test certificate

--- test/Tests/RealAuthorizationServerTest.cs
+++ test/Tests/RealAuthorizationServerTest.cs
@@ -12,7 +12,7 @@ namespace Tests
             _tokenClient = new JwtAssertionTokenClient("https://localhost:44300/connect/token", "test_client", TestCertificate.Load());
         }
 
-        [Fact]
+        [Fact(Skip = "Disable until server side implementation is released in upstream so that it can be used with NuGet")]
         public async void should_get_access_token()
         {
             var token = await _tokenClient.GetAccessTokenAsync("test_scope");
