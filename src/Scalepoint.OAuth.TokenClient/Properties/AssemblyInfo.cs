a6a24e39479114baba31d5f5fa3efe214d04473a
Yuriy Ostapenko <uncleyo@users.noreply.github.com>
2016-05-16 15:35:39 +0300

    Add nuspec

--- src/Scalepoint.OAuth.TokenClient/Properties/AssemblyInfo.cs
+++ src/Scalepoint.OAuth.TokenClient/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Scalepoint.OAuth.TokenClient")]
+[assembly: AssemblyTitle("Scalepoint OAuth Token Client")]
 [assembly: AssemblyDescription("Client helper for OAuth 2.0 Token endpoint")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Scalepoint")]
@@ -35,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
